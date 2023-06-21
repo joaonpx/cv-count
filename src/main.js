6045d1b0138b408d69f6bbda89e440024a581123
Npx <cheyprivado@gmail.com>
2023-06-21 20:37:31 -0300

    :hammer: Atualizando main script

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { addCount } from "./utils/count"
 
 let btnCount = document.querySelector("#btn-count")
+let countNumber = document.getElementById("count-number")
 
 btnCount.addEventListener("click", addCount)
+
+countNumber.innerText = localStorage.clickCount
