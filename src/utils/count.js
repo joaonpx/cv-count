e29354141e18a667e308cfa6c16177f5cbfea74c
Npx <cheyprivado@gmail.com>
2023-06-21 20:37:11 -0300

    :hammer: Atualizando script da contagem

--- src/utils/count.js
+++ src/utils/count.js
@@ -1,13 +1,16 @@
-let storage = window.localStorage
+let countNumber = document.getElementById("count-number")
 
-let countNumber = document.querySelector("#count")
+function addCount() {
+  // verificando se existe algum valor dentro do item clickCount
+  if (localStorage.clickCount) {
+    // se existir pega o valor atual e soma + 1
+    localStorage.clickCount = Number(localStorage.clickCount) + 1
+  } else {
+    // se não existir valor dentro do item clickCount adiciona 0
+    localStorage.clickCount = 0
+  }
 
-let clickCount = 0
-
-export function addCount() {
-  ++clickCount
-
-  storage.setItem("count", clickCount)
-
-  countNumber.innerHTML = storage.getItem("count")
+  countNumber.innerText = localStorage.clickCount
 }
+
+export { addCount }
